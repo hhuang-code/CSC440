0b5acd14daf0910ed543b63a8976fee5e7735660
huanghao-code <huanghao_bupt@sina.com>
2017-10-05 16:09:26 -0400

    Create libcheck.py

---
+++ Assignment-1/libcheck.py
@@ -0,0 +1,21 @@
+# Check the versions of libraries
+# Python version
+
+import sys
+print('Python: {}'.format(sys.version))
+
+# scipy - scientific computing tools for Python
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+# numpy - the fundamental package for scientific computing with Python
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+# matplotlib - a Python 2D plotting library
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas - data structures and data analysis tools for Python 
+import pandas
+print('pandas: {}'.format(pandas.__version__))
